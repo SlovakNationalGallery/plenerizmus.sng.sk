c3f71611a1ca0fccdeb16cd383a76b01f66b5e8a
Igor Rjabinin <igor.rjabinin@gmail.com>
2019-04-16 14:05:27 +0200

    change page description (og/meta)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,10 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <meta name="description" content="Počas 19. storočia objavovali umelci a umelkyne čaro tvorby v plenéri. Krásy prírody, mesta či vidieka zachytávali priamo na plátno alebo na papier. Vyber si z pestrej škály motívov, nálad a počasia a preskúmaj výslednú maľbu alebo kresbu naozaj zblízka">
+  <meta name="description" content="Vyber si z pestrej škály motívov, nálad a počasia a preskúmaj výslednú maľbu alebo kresbu naozaj zblízka.">
 
   <meta property="og:title" content="Z akadémie do prírody" />
-  <meta property="og:description" content="Počas 19. storočia objavovali umelci a umelkyne čaro tvorby v plenéri. Krásy prírody, mesta či vidieka zachytávali priamo na plátno alebo na papier. Vyber si z pestrej škály motívov, nálad a počasia a preskúmaj výslednú maľbu alebo kresbu naozaj zblízka" />
+  <meta property="og:description" content="Vyber si z pestrej škály motívov, nálad a počasia a preskúmaj výslednú maľbu alebo kresbu naozaj zblízka." />
   <meta property="og:type" content="website" />
   <meta property="og:url" content="{{ url()->current() }}" />
   <meta property="og:image" content="{{ asset('/images/og_image.jpg') }}" />
