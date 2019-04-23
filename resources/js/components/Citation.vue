0bb7713961525bf02dacff55acb0f2127fd4a4d4
Igor Rjabinin <igor.rjabinin@gmail.com>
2019-04-23 13:00:42 +0200

    fix breakpoint

--- resources/js/components/Citation.vue
+++ resources/js/components/Citation.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8 col-lg-6">
+            <div class="col-md-8 col-xxl-6">
                 <transition name="fade">
                     <blockquote v-on:click="randomCitation" v-if="show">
                         <p class="small">{{ selectedCitation.message }}</p>
